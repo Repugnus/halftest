b52fcb88d10c3ec3ada06778814df290d9f58068
Unknown <elitecookie25@gmail.com>
2017-02-18 21:57:37 -0500

    Idiot

--- hud.lua
+++ hud.lua
@@ -1,3 +1,6 @@
+local function update_health(player)
+	player:hud_change(player, "text", "health "..player:get_hp()*5)
+end
 minetest.register_on_joinplayer(function(player)
 player:hud_set_flags({ healthbar=false })
 player:hud_add({
@@ -7,13 +10,10 @@ player:hud_add({
          text = "",
          number = 0xffa500
 })
-if not minetest.setting_getbool("creative_mode") then
+if minetest.setting_getbool("enable_damage") then
 	update_health(player)
 end
 end)
-local function update_health(player)
-	player:hud_change(player, "text", "health "..player:get_hp()*5)
-end
 minetest.register_on_player_hpchange(update_health)
 minetest.register_on_respawnplayer(function(player)
 	player:hud_change(player, "text", "health "..player:get_hp()*5)
