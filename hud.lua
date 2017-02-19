786639f891795bd096982a9bfae59dfacadf428e
Unknown <elitecookie25@gmail.com>
2017-02-18 21:54:38 -0500

    Made health not show up if in creative

--- hud.lua
+++ hud.lua
@@ -4,13 +4,17 @@ player:hud_add({
          hud_elem_type = "text",
          position = {x = .04, y = .97},
          name = "health",
-         text = "health "..player:get_hp()*5,
+         text = "",
          number = 0xffa500
 })
 end)
 local function update_health(player)
 	player:hud_change(player, "text", "health "..player:get_hp()*5)
 end
+if not minetest.setting_getbool("creative_mode") then
+	update_health(player)
+end
+
 minetest.register_on_player_hpchange(update_health)
 minetest.register_on_respawnplayer(function(player)
 	player:hud_change(player, "text", "health "..player:get_hp()*5)
